34779655ab5890234bf186ea214ca5c387f91de9
jiaulislam <jiaulislam.ict.bd@gmail.com>
2023-04-17 15:06:18 +0600

    changed readme

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Jiaul Islam Portfolio';
-export const SITE_DESCRIPTION = 'A Modern portfolio built with Astro';
+export const SITE_DESCRIPTION = 'Jiaul Islam Portfolio built with Astro';
