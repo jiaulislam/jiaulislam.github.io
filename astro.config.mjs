e2bf3baeed793ef86149fff31198bc7178b3aaf4
Strativ <strativ@Strativs-MacBook-Pro.local>
2024-10-12 10:24:08 +0600

    :sparkles: adding google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import sitemap from '@astrojs/partytown';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jiaulislam.dev/',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  })],
   "plugins": [
     {
       "name": "@astrojs/ts-plugin"
